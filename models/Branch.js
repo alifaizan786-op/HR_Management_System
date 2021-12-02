1a4f3bc0f67f601fa73dc1bf59f9d57e02703451
alifaizan786-op <faizan.ali@irunadigital.com>
2021-12-01 20:13:18 -0500

    added the employee, role, branch models

---
+++ models/Branch.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require ('../config/connection');
+
+class Branch extends Model {
+}
+
+Branch.init(
+    {
+        //creating id column for the branch
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //referencing to the employee table where it will look for the employee with a manager role and has to match with the branch id
+        manager: {
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'employee',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'branch'
+    }
+);
+
+module.exports = Branch;
