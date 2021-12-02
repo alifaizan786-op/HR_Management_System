6f9f12f91c69fbd1edd91030a303f1425b60672e
Alexander VanOsdol <alex.m.vanosdol@me.com>
2021-12-02 15:43:29 -0500

    updated seed.js now populates db still need to work on linking tables together to populate data

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,42 @@ const sequelize = require('../config/connection');
 const Role = require('../models/Roles');
 const Employee = require('../models/Employee');
 const Branch = require('../models/Branch');
-const EmployeeData = require('./EmployeeData.json')
+const Benefits = require("../models/Benefits");
+
+const RoleData = require("./Roles-seed.json");
+const EmployeeData = require('./Employee-seed.json');
+const BranchData = require("./Branch-seed.json");
+const BenefitsData = require("./Benifits-seed.json");
+
+
+const seedsDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await Branch.bulkCreate(BranchData,{
+        individualHooks: true,
+    returning: true,
+    });
+
+    await Role.bulkCreate(RoleData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Employee.bulkCreate(EmployeeData,{
+        individualHooks: true,
+    returning: true,
+    });
+
+    await Benefits.bulkCreate(BenefitsData,{
+        individualHooks: true,
+    returning: true,
+    });
+    process.exit(0);
+}
+
+seedsDatabase();
+
+
 // role ids used in json seed file 
 // role IDs
 // 1:sales
