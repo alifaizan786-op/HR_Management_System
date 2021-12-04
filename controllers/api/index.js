86e9a5733ab0b3da9ecad58b1b982e20362cf8d7
alifaizan786-op <faizan.ali@irunadigital.com>
2021-12-04 10:45:32 -0500

    added routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const timeOffRoutes = require('./timeOffRoutes');
 
-router.use('/empoyee', userRoutes);
+router.use('/employee', userRoutes);
 router.use('/timeOff', timeOffRoutes);
 
 module.exports = router;
