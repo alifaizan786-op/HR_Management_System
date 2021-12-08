7d3df8dd2fcc9501af6a2cca40910b4bc6d09565
Matthew Flanagan <mattflanagan40@gmail.com>
2021-12-08 07:59:32 -0500

    login/logout function, profile page & timeOff page rendering

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,8 +1,43 @@
-// const sideNav = document.querySelector('#sideNavBar');
+var dateTime = moment().format("MMM Do, YYYY, hh:mm");
 
-// sideNav.addEventListener('onload', function () {
-//     var name = document.querySelector('#nameInput').value;
-//     var edit = document.querySelector('#editInput').value;
+console.log('connected')
 
-//     console.log(name + edit);
-// });
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  console.log(email);
+  const password = document.querySelector('#password-login').value.trim();
+  console.log(password);
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/employee/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      console.log('successful');
+      // If successful, redirect the browser to the profile page
+      document.location.assign('/profile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+$(".dateTime").text(dateTime);
+
+console.log(dateTime)
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
