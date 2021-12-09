bc4e7b9399308955dde491f91ca824d88fab7a10
Matthew Flanagan <mattflanagan40@gmail.com>
2021-12-09 08:16:31 -0500

    background & display style update

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,11 @@
-const withAuth = (req, res, next) => {
-  if (!req.session.loggedIn) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
-
-module.exports = withAuth;
+// module.exports = {
+//   // the helper method 'format_time' will take in a timestamp and return a string with only the time
+//   format_time: (date) => {
+//     // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
+//     return date.toLocaleTimeString();
+//   },
+//     isEqual: (v1, v2) =>
+//       {if(v1 === v2) {
+//         return options.fn(this);
+//       }
+//     }}
