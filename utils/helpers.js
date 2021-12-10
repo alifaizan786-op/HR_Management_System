240e417614e435d8837095880f450342d0f24f59
Matthew Flanagan <mattflanagan40@gmail.com>
2021-12-09 23:14:45 -0500

    scripts moved to viewallemp handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,3 +10,18 @@ hb.registerHelper('dateFormat', function (date, options) {
 hb.registerHelper('dateNow', () => {
     return new Date();
 });
+
+hb.registerHelper({
+    eq: (v1, v2) => v1 === v2,
+    ne: (v1, v2) => v1 !== v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    }
+});
